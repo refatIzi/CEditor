c890f594c3e90203da7bebc10082bc1ac3646bc1
Rifat Ismailov <refaazer@gmail.com>
2024-02-14 19:07:01 +0200

    Я почав створювати новий проєкт на основі мого старого проєкту.(I started creating a new project based on my old project.) Основні вимогу відокремити дій в різні біблеотеки для зручного використання. (The main requirement is to separate actions into different libraries for convenient use.)

--- app/src/main/java/com/example/ceditor/MainActivity.java
+++ app/src/main/java/com/example/ceditor/MainActivity.java
@@ -11,7 +11,10 @@ import com.example.ceditors.language.Language;
 import com.example.generate.Generate;
 import com.example.generate.ReturnActivity;
 
-
+/**
+ * @Activity Реалізує інтерфейс ReturnActivity:
+ * Цей інтерфейс містить методи для повернення даних з активності.
+ **/
 public class MainActivity extends AppCompatActivity implements ReturnActivity {
 
     TextView texttt;
@@ -21,13 +24,20 @@ public class MainActivity extends AppCompatActivity implements ReturnActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         texttt = findViewById(R.id.texttt);
+        /**
+         * Створює масив рядків functions, який містить список функцій Python.
+         * Створює об'єкт класу Language з кодом Python, мовою та списком функцій.
+         * Створює об'єкт класу Generate, який використовується для виконання дії з класом CEditors який є Activity та об'єктом Language.
+         **/
 
-        String[] functions = {"self", "def", "as", "assert", "break", "continue", "del", "elif", "else", "except", "finally", "for", "from", "global", "if", "import", "in", "pass", "raise", "return", "try", "while", "with", "yield", "min()", "setattr()", "abs()", "all()", "dir()", "hex()", "next()", "any()", "divmod()", "id()", "sorted()", "ascii()", "enumerate()", "input()", "oct()", "max()", "round()",
-                "bin()", "eval()", "exec()", "isinstance()", "ord()", "sum()", "filter()", "issubclass()", "pow()", "iter()", "print()", "callable()", "format()", "delattr()",
-                "len()", "chr()", "range()", "vars()", "getattr()", "locals()", "repr()", "zip()", "compile()", "globals()", "map()", "reversed()", "__import__()", "hasattr()", "hash()", "memoryview()"};
-
+        String[] functions = {
+                "self", "def", "as", "assert", "break", "continue", "del", "elif", "else", "except", "finally", "for", "from", "global",
+                "if", "import", "in", "pass", "raise", "return", "try", "while", "with", "yield", "min()", "setattr()", "abs()", "all()",
+                "dir()", "hex()", "next()", "any()", "divmod()", "id()", "sorted()", "ascii()", "enumerate()", "input()", "oct()", "max()",
+                "round()", "bin()", "eval()", "exec()", "isinstance()", "ord()", "sum()", "filter()", "issubclass()", "pow()", "iter()",
+                "print()", "callable()", "format()", "delattr()", "len()", "chr()", "range()", "vars()", "getattr()", "locals()", "repr()",
+                "zip()", "compile()", "globals()", "map()", "reversed()", "__import__()", "hasattr()", "hash()", "memoryview()"};
         Language language = new Language("print(\"Hello World HI RIFATE\")", "Python", functions);
-
         new Generate(this, CEditors.class, language, "language");
 
 
